75f67a18372ee94373d96aff8c93d0bb0b1da8c3
TahaYasin <taha_ysnn@hotmail.com>
2026-03-12 09:16:37 +0300

    playground

---
+++ Playground/Variables&Constants.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+// Variables & Constants
+
+
+// snake_case
+// camelCase
+
+var userName = "James"
+
+print(userName)
+
+userName = "John"
+
+print(userName)
+ 
+let userAge = 30
+
+// userAge = 45 sabit degisken
+
+
+
+
